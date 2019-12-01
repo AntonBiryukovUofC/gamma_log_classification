d39448920590db9279ee814b01ea47efcba031dd
Pavel Pleskov <ppleskov#gmail.com>
2019-12-01 21:05:13 +0300

    gpu fix

---
+++ 2.sh
@@ -0,0 +1,2 @@
+python src/models/train_model_fpn_batch.py --epochs 300 --fold 2 --gpu 2 --dropout 0.1
+python src/models/train_model_fpn_batch.py --epochs 300 --fold 3 --gpu 2 --dropout 0.1
---
+++ 3.sh
@@ -0,0 +1,1 @@
+python src/models/train_model_fpn_batch.py --epochs 300 --fold 4 --gpu 3 --dropout 0.1 --batch_size 64
