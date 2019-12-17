855f3c99cdb67f51b18b12271396cb2788a8d9f0
Anton Biryukov <anton.biryukov@ucalgary.ca>
2019-12-17 00:07:58 -0700

    added trend

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ from keras.backend.tensorflow_backend import set_session
 @click.command()
 @click.option('--start_fold', default=0, help='fold to train')
 @click.option('--gpu', default=0, help='gpu to train on')
-@click.option('--add_trend', default=False, help='add trend to xstarter ?')
+@click.option('--add_trend', help='add trend to xstarter ?',is_flag=True)
 def main(start_fold,gpu,add_trend):
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
@@ -22,7 +22,8 @@ def main(start_fold,gpu,add_trend):
     config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
     sess = tf.Session(config=config)
     set_session(sess)  # set this TensorFlow session as the default session for Keras
-
+    if add_trend:
+        log.info('Will add trend to XEEK Train data')
     GetData = DataGenerator(add_trend=add_trend)
     CV = Pipeline(GetData, DL_model, start_fold, gpu)
     score = CV.train()
