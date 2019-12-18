2b2cd8804b17ac9f411bff17e8dfddee7b4166b4
Anton Biryukov <anton.biryukov@ucalgary.ca>
2019-12-18 01:22:50 -0700

    added trend

--- Pipeline.py
+++ Pipeline.py
@@ -119,9 +119,9 @@ class Pipeline():
             if fold != self.start_fold:
                 continue
 
-            _, _, X_val, y_val = self.GetData.get_train_val(train_ind, val_ind)
+           # _, _, X_val, y_val = self.GetData.get_train_val(train_ind, val_ind)
 
-            gen_function = batch_generator(X_train,y_train,X_train_xstart,y_train_xstart,batch_size=64)
+            gen_function = batch_generator(X_train,X_train_xstart,y_train,y_train_xstart,batch_size=32)
             checkpointer = ModelCheckpoint(self.model_name + '_' + str(fold) + f'_{self.gpu}.h5',
                                            monitor='val_accuracy',
                                            mode='max', verbose=1, save_best_only=True)
@@ -133,8 +133,7 @@ class Pipeline():
 
             # train model
             history = self.model.fit_generator(generator=gen_function,  epochs=self.epochs,steps_per_epoch=7000 // 64,
-                                     callbacks=[self.earlystopper, checkpointer],
-                                     validation_data=(X_val, y_val))
+                                     callbacks=[self.earlystopper, checkpointer,self.reduce_lr])
 
             pred_val[val_ind, :, :] = self.model.predict(X_val)
 
